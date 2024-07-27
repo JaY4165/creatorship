8e67d5d13c8958c9cee6e23a4116235e8e520e33
JaY4165 <harsha.gontu@gmail.com>
2024-07-27 23:32:04 +0530

    prisma migrations

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -35,17 +35,16 @@ export async function updateSession(request: NextRequest) {
     if (
         !user &&
         !request.nextUrl.pathname.startsWith('/login') &&
-        !request.nextUrl.pathname.startsWith('/auth')
+        !request.nextUrl.pathname.startsWith('/sign-up')
     ) {
         const url = request.nextUrl.clone()
         url.pathname = '/login'
         return NextResponse.redirect(url)
     }
 
-    if (user && (request.nextUrl.pathname.startsWith('/login') || request.nextUrl.pathname.startsWith('/auth'))) {
-        const url = request.nextUrl.clone()
-        url.pathname = '/'
-        return NextResponse.redirect(url)
+    if(user)
+    {
+        
     }
 
     return supabaseResponse
